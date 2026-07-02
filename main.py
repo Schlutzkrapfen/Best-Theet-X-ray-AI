d6bccbfdec8afef75e4262634cb9876fb1ddf600
Schlutzkrapfen <david.messner@protonmail.com>
2026-07-02 09:56:45 +0200

    now it saves the first step works

--- main.py
+++ main.py
@@ -18,12 +18,9 @@ def try_Ai(model: str, input: str, output_folder: str = "./Results"):
     filename, _ = os.path.splitext(os.path.basename(input))
     save_path = os.path.join(output_folder, f"{filename}.txt")
     results: list[ultralytics.engine.results.Results] = yolo_model(input, save_txt=None)
-    with open(save_path) as file:
-        for idx, prediction in enumerate(
-            results[0].boxes.xywhn
-        ):  # change final attribute to desired box format
+    with open(save_path, "w") as file:
+        for idx, prediction in enumerate(results[0].boxes.xywhn):
             cls = int(results[0].boxes.cls[idx].item())
-            # Write line to file in YOLO label format : cls x y w h
             file.write(
                 f"{cls} {prediction[0].item()} {prediction[1].item()} {prediction[2].item()} {prediction[3].item()}\n"
             )
@@ -42,8 +39,14 @@ def show_results(results):
 
 def main():
     for model_file in os.listdir(MODEL_FOLDER):
+        _, end = os.path.splitext(os.path.basename(model_file))
+        if end != ".pt":
+            continue
         model_path = os.path.join(MODEL_FOLDER, model_file)
         for image_file in os.listdir(INPUT_FOLDER):
+            _, end = os.path.splitext(os.path.basename(image_file))
+            if end != ".jpg" and end != ".png":
+                continue
             image_path = os.path.join(INPUT_FOLDER, image_file)
             try_Ai(model_path, image_path, RESULT_FOLDER)
 
